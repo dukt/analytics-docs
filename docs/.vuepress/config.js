6cdfe53a2daac2b630af15e30f3e004c6f952368
Benjamin David <ben@dukt.net>
2021-03-04 14:18:50 +0100

    Updated to `vuepress` 1.8

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,14 @@ module.exports = {
     title: 'Analytics Documentation',
     description: 'Analytics Documentation',
     base: '/docs/analytics/v4/',
-    ga: 'UA-1547168-20',
+    plugins: [
+        [
+            '@vuepress/google-analytics',
+            {
+                'ga': 'UA-1547168-20'
+            }
+        ]
+    ],
     themeConfig: {
         docsRepo: 'dukt/analytics-docs',
         docsDir: 'docs',
